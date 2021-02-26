d159bd7bc8c94e064a028fed65049592e3419ebc
Lakhte <lakhteagha@gmail.com>
2021-02-26 13:50:03 -0500

    halfway thru todo-list

--- vanillaDomManipulation/src/todo_list.js
+++ vanillaDomManipulation/src/todo_list.js
@@ -1,2 +1,23 @@
+const todos = [];
 
+const ul = document.querySelector(".todos");
+const form = document.querySelector(".add-todo-form");
 
+function addTodo() {
+  const currentTodoValue = document.getElementsByName("add-todo").value();
+  const todo = {
+    value: currentTodoValue,
+    done: false,
+  };
+  todos.push(todo);
+  document.getElementsByName("add-todo").value() = "";
+}
+
+function populateList(arr) {
+  arr.forEach((todo) => {
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.value = todo.value;
+  });
+}
