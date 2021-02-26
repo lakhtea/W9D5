dfa733af865ec21784b96c34e50403be5aff11c2
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-02-26 16:02:12 -0500

    Broke everything

--- vanillaDomManipulation/src/index.js
+++ vanillaDomManipulation/src/index.js
@@ -1,3 +1,4 @@
 import "./warmup";
 import "./clock";
 import "./drop_down";
+import "./todo_list";
--- vanillaDomManipulation/src/todo_list.js
+++ vanillaDomManipulation/src/todo_list.js
@@ -1,23 +1,71 @@
-const todos = [];
+let todos
+
+if (localStorage.getItem("todos")) {
+  todos = JSON.parse(localStorage.getItem("todos"));
+} else {
+  todos = [];
+}
 
 const ul = document.querySelector(".todos");
 const form = document.querySelector(".add-todo-form");
 
-function addTodo() {
-  const currentTodoValue = document.getElementsByName("add-todo").value();
+function addTodo(event) {
+  event.preventDefault();
+  const currentTodoValue = document.getElementsByName("add-todo")[0].value;
   const todo = {
     value: currentTodoValue,
     done: false,
   };
   todos.push(todo);
-  document.getElementsByName("add-todo").value() = "";
+  document.getElementsByName("add-todo")[0].value = "";
+  const JSONArray = JSON.stringify(todos);
+  localStorage.setItem('todos', JSONArray);
+  populateListItem(todo, todos.length-1);
+}
+
+function populateListItem(item, idx) {
+  const label = document.createElement("label");
+  const input = document.createElement("input");
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+
+  span.innerHTML = item.value;
+  input.type = "checkbox";
+  input.setAttribute("data-id", idx)
+
+  if (item.done) {
+    input.checked = true
+  }
+
+  label.append(input);
+  label.append(span);
+  li.append(label);
+  document.querySelector(".todos").append(li);
 }
 
 function populateList(arr) {
-  arr.forEach((todo) => {
-    const label = document.createElement("label");
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.value = todo.value;
+  arr.forEach((todo, idx) => {
+    populateListItem(todo, idx);
   });
 }
+
+function toggleCheck(event) {
+  debugger
+  event.currentTarget.checked = !event.currentTarget.checked
+  // todos[event.currentTarget]
+}
+
+// add event listener to after DOM loaded
+
+const checkies = Array.from(document.querySelectorAll(".todos label"));
+
+for (let i = 0; i < checkies.length; i++) {
+  debugger
+  checkies[i].addEventListener("click", toggleCheck);
+}
+
+console.log(checkies);
+
+
+document.querySelector(".add-todo-form").addEventListener("submit", addTodo);
+populateList(todos);
